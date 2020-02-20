7ef373daf3818e6c42d8a46c509b6eef98c9bc7d
Liuhjhj <liu2445342557@gmail.com>
2020-02-20 20:10:39 +0800

    Fix Bug

--- src/com/ui/Login.java
+++ src/com/ui/Login.java
@@ -13,6 +13,8 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
@@ -33,26 +35,7 @@ public class Login implements Initializable{
     public Stage primaryStage;
 
     public void loginAction(ActionEvent actionEvent) {
-        Connection connection = Sql.connect(username.getText(), password.getText());
-        if (connection != null){
-            System.out.println("SQL Database Connected!");
-            Sql.disconnect(connection);
-            Stage stage = new Stage();
-            Parent parent = null;
-            try {
-                parent = FXMLLoader.load(getClass().getResource("/com/ui/Menu.fxml"));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            stage.setScene(new Scene(parent));
-            stage.setTitle("人事管理系统");
-            stage.show();
-            if (primaryStage == null)
-                primaryStage = (Stage)rootLayout.getScene().getWindow();
-            primaryStage.close();
-        }else {
-            AlertDiaog.alert("登录数据库失败", "用户名或密码错误");
-        }
+        loginDatabase();
     }
 
     @Override
@@ -65,5 +48,38 @@ public class Login implements Initializable{
         if (primaryStage == null)
             primaryStage = (Stage)rootLayout.getScene().getWindow();
         primaryStage.close();
+    }
+
+    public void loginKey(KeyEvent keyEvent) {
+        if (keyEvent.getCode() == KeyCode.ENTER){
+            loginDatabase();
+        }
+    }
+
+    public void loginDatabase(){
+        Connection connection = Sql.connect(username.getText(), password.getText());
+        if (connection != null){
+            System.out.println("SQL Database Connected!");
+            Sql.disconnect(connection);
+            String[] info = {username.getText(), password.getText()};
+            try {
+                Stage stage = new Stage();
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/ui/Menu.fxml"));
+                Parent parent = loader.load();
+                Menu menu = loader.getController();
+                menu.userLabel.setText("尊敬的"+username.getText()+", 您好!");
+                stage.setScene(new Scene(parent));
+                stage.setTitle("人事管理系统");
+                stage.setUserData(info);
+                stage.show();
+                if (primaryStage == null)
+                    primaryStage = (Stage)rootLayout.getScene().getWindow();
+                primaryStage.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }else {
+            AlertDiaog.alert("登录数据库失败", "用户名或密码错误");
+        }
     }
 }
--- src/com/ui/Menu.java
+++ src/com/ui/Menu.java
@@ -1,13 +1,41 @@
 package com.ui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Menu implements Initializable {
+    public Stage primaryStage;
+    public BorderPane rootLayout;
+    public Label userLabel;
+    public Button exit;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
     }
+
+
+    public void exitAction(ActionEvent actionEvent) {
+        try {
+            Stage stage = new Stage();
+            Parent parent = FXMLLoader.load(getClass().getResource("/com/ui/Login.fxml"));
+            stage.setScene(new Scene(parent));
+            stage.setTitle("人事管理系统");
+            stage.show();
+            if (primaryStage == null)
+                primaryStage = (Stage) rootLayout.getScene().getWindow();
+            primaryStage.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
 }
